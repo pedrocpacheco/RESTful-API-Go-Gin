677453badb47ae256ac0369a6730ec4879449de5
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-22 11:00:47 -0300

    feat: in memory database of albums

--- main.go
+++ main.go
@@ -6,3 +6,9 @@ type album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Amado Amigo", Artist: "Guga Xavier", Price: 38.40},
+	{ID: "3", Title: "Fogo em Meus Olhos", Artist: "Alessandro Villas Boas", Price: 18.40},
+}
